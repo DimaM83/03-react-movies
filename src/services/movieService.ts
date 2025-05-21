749dc0481d4119b53e559d8c2a6ec53db6cf5aef
DimaM83 <mdv16.01.83@gmail.com>
2025-05-21 22:37:21 +0200

    add movie

---
+++ src/services/movieService.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { Movie } from '../types/movie';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+interface SearchMoviesResponse {
+  results: Movie[];
+}
+
+export async function fetchMovies(query: string): Promise<Movie[]> {
+  const response = await axios.get<SearchMoviesResponse>(`${BASE_URL}/search/movie`, {
+    params: {
+      query,
+      include_adult: false,
+      language: 'en-US',
+      page: 1,
+    },
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+    },
+  });
+
+  return response.data.results;
+}
